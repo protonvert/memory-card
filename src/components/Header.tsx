42eb61eeb59cd3ad4ba3621a93dcb9dff6313555
tristen <79072811+protonvert@users.noreply.github.com>
2023-08-25 14:58:19 -0500

    add best score

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react"
+
 export default function Header({score}) {
     
+    const [highScore, setHighScore] = useState(0)
+
+    if(score > highScore) {
+        setHighScore(score)
+    }
+
     return (
         <div className="">
             <h1>Memory Card Game</h1>
+            <span>Best Score: {highScore}</span>
+            <br />
             <span>Score: {score}</span>
         </div>
     )
